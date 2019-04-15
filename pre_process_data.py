dbeb8a56b949e5bebae321c90dea7e6fb095d28a
Henchhing Limbu <henchhing@henchhing-macbookpro2.roam.corp.google.com>
2019-04-15 15:05:46 -0700

    add script to produce training and testing csv file

--- pre_process_data.py
+++ pre_process_data.py
@@ -1,4 +1,6 @@
+import csv
 import os
+import pandas as pd
 from keras.preprocessing import image
 
 def pre_process_data(directory, label):
@@ -10,8 +12,26 @@ def pre_process_data(directory, label):
                                  grayscale=True)
             img = image.img_to_array(img)
             img = img / 255
-            processed_data.append((img, label))
+            processed_data.append([img, label])
         else:
             continue
     return processed_data
 
+bloodcell_subtypes = {
+	"LYMPHOCYTE":   [1,0,0,0]
+	, "EOSINOPHIL": [0,1,0,0]
+	, "MONOCYTE":   [0,0,1,0]
+	, "NEUTROPHIL": [0,0,0,1]
+}
+def create_dataset(directory, data_type):
+	processed_data = []
+	# creating a csv file
+    	# go through the directory
+	for key, val in bloodcell_subtypes.items():
+		processed_data += pre_process_data(directory + "/" + key, val)
+	print("Length of processed data: " + str(len(processed_data)))
+	df = pd.DataFrame(processed_data, columns=["Input","Label"])		
+		 
+	df.to_csv(data_type + ".csv" , index=False)	
+
+create_dataset("/Users/henchhing/Downloads/blood-cells/dataset2-master/images/TEST", "testing")   
